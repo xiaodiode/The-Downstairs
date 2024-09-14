a974d0431a9007a600d288d8b84ae2c0d9eb42fb
xiaodiode <44079632+xiaodiode@users.noreply.github.com>
2024-09-14 03:35:28 -0500

    set up sidescroller bedroom view and movement
    player can only use horizontal movement in sidescroller mode, bedroom has wall collisions, there is a foreground layer in bedroom

---
+++ The Downstairs/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D playerRB;
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private Vector2 cameraXRange;
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private bool isTopdown;
+    
+    private float horizontalInput, verticalInput;
+    private Vector3 velocity;
+    private Vector3 newPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>(); 
+
+        velocity = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    private void move(){
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        if(!isTopdown){
+            velocity.x = (float)Math.Round(horizontalInput)*playerSpeed;
+        }
+        
+
+        else if(isTopdown){
+            velocity.y = (float)Math.Round(verticalInput)*playerSpeed;
+        }
+
+        playerRB.velocity = velocity;
+
+        newPosition = playerRB.transform.position;
+        newPosition.y = playerCamera.transform.position.y;
+        newPosition.z = playerCamera.transform.position.z;
+
+        playerCamera.transform.position = newPosition;
+    }
+
+
+}
