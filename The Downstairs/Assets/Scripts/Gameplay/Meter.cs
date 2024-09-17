60702099645a25275fdc96c45349d00c9a630722
xiaodiode <44079632+xiaodiode@users.noreply.github.com>
2024-09-17 01:09:29 -0500

    meters can decrease continuously and change value dynamically
    meter class created, all meters can be programmed to start decreasing continuously and decrease/increase in set increments during runtime

---
+++ The Downstairs/Assets/Scripts/Gameplay/Meter.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Meter : MonoBehaviour
+{
+    public bool isEmpty, meterEnabled;
+    [SerializeField] private Slider meter;
+    [SerializeField] private TextMeshProUGUI meterValue;
+    [SerializeField] private float secondsToEmpty;
+
+    private float startTime, timePassed;
+    private float currValue;
+
+    private bool inCoroutine;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isEmpty = false;
+        meterEnabled = false;
+
+        currValue = 100;
+
+        meterValue.text = Mathf.CeilToInt(meter.value).ToString();
+
+        //debugging
+        // meter.value = 80;
+        // startDecreasing();
+
+        // StartCoroutine(waitForSeconds(2));
+
+        
+    }
+
+    private IEnumerator waitForSeconds(float seconds){
+        // inCoroutine = true;
+        
+        yield return new WaitForSeconds(seconds);
+        changeByAmount(true, 20);
+        startDecreasing();
+
+        // inCoroutine = false;
+        // startDecreasing();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(!inCoroutine){
+            if(!isEmpty && meterEnabled){
+                decreaseMeter();
+            }  
+        // }
+        
+    }
+
+    public void startDecreasing(){
+        startTime = Time.time;
+        currValue = meter.value;
+
+        meterEnabled = true;
+    }
+
+    public void stopDecreasing(){
+
+        meterEnabled = false;
+
+    }
+
+    public void makeMeterFull(){
+        stopDecreasing();
+
+        meter.value = meter.maxValue;
+    }
+
+    public void changeByAmount(bool decrease, float amount){
+        
+        stopDecreasing();
+
+        if(decrease){
+            meter.value -= amount;
+
+            if(meterEnabled){
+                startDecreasing();
+            }
+        }
+        else{
+            meter.value += amount;
+
+            if(meter.value > meter.maxValue){
+                meter.value = meter.maxValue;
+            }
+        }
+    }
+
+    private void decreaseMeter(){
+        timePassed = (secondsToEmpty*(meter.maxValue - currValue)/meter.maxValue) + (Time.time - startTime);
+
+        if(meter.value >= 0){
+            meter.value = meter.maxValue - (timePassed/secondsToEmpty)*meter.maxValue;
+            meterValue.text = Mathf.CeilToInt(meter.value).ToString();
+        }
+        else{
+            meter.value = 0;
+            isEmpty = true;
+
+            stopDecreasing(); 
+        }
+        
+    }   
+}
---
+++ The Downstairs/Assets/Scripts/Systems/MetersController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MetersController : MonoBehaviour
+{
+    [Header("Hunger Meter")]
+    [SerializeField] private Slider hungerMeter;
+    [SerializeField] private float hungerDecreaseRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
