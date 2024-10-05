442eaefe7f4fcebfa96d800889e3f8b7f5525c86
Maxwell Yang <meyang2@illinois.edu>
2024-10-05 13:14:36 -0500

    Settings Downstairs

---
+++ The Downstairs/Assets/Scripts/Aesthetics/InstantFont.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+
+public class InstantFont : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float timeDilate;
+    private TextMeshProUGUI tmp;
+
+    void OnEnable()
+    {
+        tmp = gameObject.GetComponent<TextMeshProUGUI>();
+        tmp.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, -1.0f);
+        StartDilation();
+    }
+
+    void SetFaceDilate(float x)
+    {
+        tmp.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, x);
+    }
+
+    float GetFaceDilate()
+    {
+        return tmp.fontMaterial.GetFloat(ShaderUtilities.ID_FaceDilate);
+    }
+
+    public void StartDilation()
+    {
+        DOTween.To(GetFaceDilate, SetFaceDilate, 0.0f, timeDilate);
+    }
+
+    public void EndDilation()
+    {
+        DOTween.To(GetFaceDilate, SetFaceDilate, -1.0f, timeDilate);
+    }
+
+}
