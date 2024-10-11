19f068b440fd6865312aace9a703e593a176544b
Maxwell Yang <meyang2@illinois.edu>
2024-10-10 19:25:08 -0500

    Squashed commit of the following:
    commit fbd6bd4b83a13ff8671b33e46acea6287a200ed5
    Author: Maxwell Yang <meyang2@illinois.edu>
    Date:   Thu Oct 10 19:24:56 2024 -0500
    
        QTE arrows
    
    commit 939d82b42c9b7de00853a10a978d466deac24783
    Author: Maxwell Yang <meyang2@illinois.edu>
    Date:   Thu Oct 10 01:54:57 2024 -0500
    
        QTE complete (boxes)
    
    commit cf0af9585a5477853ba95c9a13a9f3d9addeae70
    Author: Maxwell Yang <meyang2@illinois.edu>
    Date:   Sat Oct 5 16:12:48 2024 -0500
    
        QTE Functionality

---
+++ The Downstairs/Assets/Scripts/QTEController.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required for working with UI components like Slider
+using DG.Tweening;
+
+public class QTEController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> keys;
+    [SerializeField] private int numberKeys;
+    [SerializeField] private Slider qteTimerSlider;  
+
+    public Transform qteSquare;
+
+    public GameObject[] qteObjects;
+    public int[] qteKeys;
+    private float horizontalInput, verticalInput;
+    private Vector3 input;
+    float time = 0f;
+    public float timeDelay = 1.0f;
+    public int current = 0;
+    public float qteTimeLimit = 3.0f; // Time limit for each QTE
+    private float qteTimer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        qteObjects = new GameObject[numberKeys];
+        qteKeys = new int[numberKeys];
+
+        // Initialize qteObjects and qteKeys
+        for (int i = 0; i < numberKeys; i++)
+        {
+            int randomIndex = Random.Range(0, 4);
+            qteObjects[i] = Instantiate(keys[randomIndex], this.transform);
+            qteKeys[i] = randomIndex;
+        }
+
+        qteTimer = qteTimeLimit;  
+        qteTimerSlider.maxValue = qteTimeLimit;  
+        qteTimerSlider.value = qteTimeLimit;  
+
+        MoveToCurrentPosition(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If all QTEs are completed, do nothing
+        if (current >= numberKeys)
+        {
+            return;
+        }
+
+        // Gather player input
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // Update QTE timer
+        qteTimer -= Time.deltaTime;
+        qteTimerSlider.value = qteTimer;   
+        if (qteTimer <= 0 && current < numberKeys)
+        {
+            Debug.Log("FAILED QTE");  
+            MoveToNextQTE(); 
+        }
+ 
+        Move();
+    }
+
+    private void Move()
+    {
+        if (time > 0f)
+        {  
+            time -= Time.deltaTime;
+            return;
+        }
+
+        if (horizontalInput == 1 && qteKeys[current] == 3)
+        {
+            // D key
+            MoveToNextQTE();
+        }
+        else if (horizontalInput == -1 && qteKeys[current] == 1)
+        {
+            // A key
+            MoveToNextQTE();
+        }
+        else if (verticalInput == 1 && qteKeys[current] == 0)
+        {
+            // W key
+            MoveToNextQTE();
+        }
+        else if (verticalInput == -1 && qteKeys[current] == 2)
+        {
+            // S key
+            MoveToNextQTE();
+        }
+    }
+
+    private void MoveToNextQTE()
+    {
+        current++;
+
+        if (current < numberKeys)
+        {
+            this.transform.DOLocalMoveX(-current * 85.0f, .5f);
+            qteTimer = qteTimeLimit;
+            qteTimerSlider.value = qteTimeLimit; 
+            time = timeDelay; 
+            qteSquare.DOLocalMoveY(10f, 0.25f).From().SetEase(Ease.OutBack);
+        }
+        else
+        {
+            Debug.Log("QTE Completed");  
+        }
+    }
+
+    private void MoveToCurrentPosition()
+    {
+        this.transform.DOLocalMoveX(-current * 85.0f, 0.5f);
+    }
+}
